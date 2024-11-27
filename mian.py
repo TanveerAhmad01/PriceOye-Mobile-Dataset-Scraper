a1e284a6b8afb3279e22de6ba68ea1098d82ccca
TanveerAhmad01 <150953314+TanveerAhmad01@users.noreply.github.com>
2024-11-27 04:21:06 -0800

    fix  orignal price added into discount

--- mian.py
+++ mian.py
@@ -14,6 +14,8 @@ class MobileScraper:
         self.discount_price_list = []
         self.original_price_list = []
         self.off_on_every_mobile_list = []
+        self.discount_price = 0
+        self.orignalPrice = 0
         self.header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
         }
@@ -32,7 +34,7 @@ class MobileScraper:
             for product in content:
                 self.extract_product_data(product)
 
-            time.sleep(2)
+            time.sleep(3)
 
     def extract_product_data(self, product):
         # Title
@@ -41,42 +43,61 @@ class MobileScraper:
         
         # Rating
         rating = product.find('span', class_='h6 bold')
-        self.rating_list.append(rating.text.strip() if rating else None)
+        getTitle = title.text.strip()
+        print("Title: ",getTitle)
+        self.rating_list.append(float(rating.text.strip()) if rating else None)
         
         # Reviews
         review = product.find('span', class_='rating-h7 bold')
         if review:
-            number = re.search(r'\d+', review.text) 
-            self.reviews_list.append(number.group() if number else None)
+            number = review.text.split()[0]
+            print("reviews: ",number)
+            self.reviews_list.append(number if number else None)
         else:
             self.reviews_list.append(None)
         
         # Discount Price
         discount_price = product.find('div', class_='price-box p1')
-        if discount_price:
-            number = re.search(r'\d+', discount_price.text)
-            self.discount_price_list.append(number.group() if number else None)
+        if discount_price: 
+            number = discount_price.text.split()[1]
+            removeComa = number.replace(',', '')
+            self.discount_price = int(removeComa)
         else:
-            self.discount_price_list.append(None)
-        
-        # Original Price
+            self.discount_price = 0
+
         original_price = product.find('div', class_='price-diff-retail')
         if original_price:
-            number = re.search(r'\d+', original_price.text)
-            self.original_price_list.append(number.group() if number else None)
+            number = original_price.text.split()[1]
+            orignal_nubmer = original_price.text.split()[1]
+            removeComaorignal = orignal_nubmer.replace(',', '')
+            self.orignalPrice = int(removeComaorignal)
         else:
-            self.original_price_list.append(None)
+            self.orignalPrice = 0
+            
         
+        if self.discount_price < self.orignalPrice:
+            print("if conldition discounted price: ",self.discount_price,self.orignalPrice)
+            self.original_price_list.append(self.orignalPrice if self.orignalPrice else None)
+            self.discount_price_list.append(self.discount_price if self.discount_price else None)   
+        else:
+            print("else conldition discounted price: ",self.discount_price)
+            self.original_price_list.append(self.discount_price if self.discount_price else None)
+            self.discount_price_list.append(None)
+        
+
         # Discount Percentage
         discount = product.find('div', class_='price-diff-saving')
         if discount:
-            number = re.search(r'\d+', discount.text)
-            self.off_on_every_mobile_list.append(number.group() if number else None)
+            number = discount.text.split()[0]
+            print("off price: ",number)
+            self.off_on_every_mobile_list.append(number if number else None)
         else:
             self.off_on_every_mobile_list.append(None)
 
-    def save_to_csv(self, filename):
-        df = pd.DataFrame({
+        print("---------------------------------------------------")
+
+    def save_to_csv(self,filename='mobile_data.csv'):
+        data= pd.DataFrame({
             'Title': self.title_list,
             'Rating': self.rating_list,
             'Reviews': self.reviews_list,
@@ -85,13 +106,12 @@ class MobileScraper:
             'Off': self.off_on_every_mobile_list
         })
         
-        df.to_csv(filename, index=False)
+        data.to_csv(filename, index=False)
         print(f"\nData saved to {filename}")
-        df
-
+        
 
 base_url = 'https://priceoye.pk/mobiles'
 max_pages = 23
 obj = MobileScraper(base_url, max_pages)
 obj.scrape()
-obj.save_to_csv('mobiles_data.csv')
+obj.save_to_csv()
